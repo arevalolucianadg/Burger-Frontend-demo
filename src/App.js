f1f12fc395c53bfa2ccb68d77bda67d5e105f672
Luciana <arevalolucianadg@gmail.com>
2021-01-28 20:55:39 -0300

    Agrego react-router-dom + Navbar + Pages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+import AddPizza from './pages/AddPizza';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <div className="container mt-4">
+          <Switch>
+            <Route path='/' exact component={Home} />
+            <Route path='/agregar-pizza' component={AddPizza} />
+          </Switch>
+        </div>
+      </>
+    </Router>
   );
 }
 
---
+++ src/pages/AddPizza.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddPizza = () => {
+    return (
+        <>
+            <h1>Agregar Pizza</h1>
+        </>
+    );
+}
+ 
+export default AddPizza;
